a786e592f367cc2e74beac47878c106984b8ffd4
rinij <rinimani7@gmail.com>
2024-10-09 16:16:19 -0600

    stop command removed

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ import json
 import turtle
 import urllib.request
 import time
-import webbrowser
 import geocoder
 import subprocess
-
-#http://open-notify.org/Open-Notify-API/People-In-Space/
 
 # URL for the API providing information about people in space
 url = "http://api.open-notify.org/astros.json"
@@ -26,20 +23,15 @@ with open("iss.txt", "w") as file:
     g = geocoder.ip('me')
     file.write("\nYour current lat/long is: " + str(g.latlng) + "\n")
 
-# The file is automatically closed when the 'with' block ends
-
 # Open the file using the default application on macOS
 subprocess.run(["open", "iss.txt"])
 
-
-#set up the world map in turtle module
-
+# Set up the world map in turtle module
 screen = turtle.Screen()
 screen.setup(1280,720)
-screen.setworldcoordinates(-180,-90,180,90)
+screen.setworldcoordinates(-180,-90,180,90)  # Set world coordinates to longitude/latitude bounds
 
-
-#load the world map image
+# Load the world map image
 screen.bgpic("world-map.gif")
 screen.register_shape("iss-icon.gif")
 iss = turtle.Turtle()
@@ -47,33 +39,23 @@ iss.shape("iss-icon.gif")
 iss.setheading(45)
 iss.penup()
 
-# Keep the window open
-#turtle.done()
-
-#to keep window open
-input('stop')
-
 while True:
-    # Lo"ad the current status of the ISS in real-time
-    url="http://api.open-notify.org/iss-now.json"
+    # Load the current status of the ISS in real-time
+    url = "http://api.open-notify.org/iss-now.json"
     response = urllib.request.urlopen(url)
     result = json.loads(response.read())
 
     # Extract ISS location
     location = result["iss_position"]
-    lat = location['latitude']
-    lon = location['longitude']
+    lat = float(location['latitude'])
+    lon = float(location['longitude'])
 
-    #Output long and lat to the terminal
-    lat = float(lat)
-    lon = float(lon)
+    # Output long and lat to the terminal
     print("\nLatitude:" + str(lat))
-    print("\nLongitude:" + str(lon))
+    print("Longitude:" + str(lon))
 
-    #Update ISS location on the map
+    # Update ISS location on the map, correcting for turtle screen coordinates
     iss.goto(lon, lat)
 
-    #Refresh each 5 sec
+    # Refresh every 5 seconds
     time.sleep(5)
-
-
